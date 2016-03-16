b92d5851c83eacc01c9fadec9b4f4719140b9ad8
Ben Lee <leeohsunshine@gmail.com>
2016-03-16 18:18:16 -0400

    fixed the width of the tabs-wrapper

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 import $ from "jquery";
 
 $(".tab-wrapper > div").on("click", function(event){
-	$(".tab-wrapper > div").removeClass("show")
-	$(".tab-wrapper > div").addClass("hidden")
+	$(".tab-wrapper > div").removeClass("show");
+	$(".tab-wrapper > div").addClass("hidden");
 	$(event.currentTarget).addClass("show");
 });
 
