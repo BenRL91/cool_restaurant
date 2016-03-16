27d2a192fc3ac7d563c487a9c426009ea538e1d6
Ben Lee <leeohsunshine@gmail.com>
2016-03-16 17:10:44 -0400

    Created javascript to show and hide tabs correctly
    added .DS_Store to gitignore file

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 // Javascript Entry Point
 import $ from "jquery";
+
+$(".tab-wrapper").on("click", function(event){
+	$(".tab-wrapper > div").removeClass("show")
+	$(event.currentTarget).addClass("show");
+});
