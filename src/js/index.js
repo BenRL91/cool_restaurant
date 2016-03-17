796d46f443f561e7d7670d74ff352d6ada65134c
Mallerie Shirley <mallerie@gmail.com>
2016-03-17 10:52:25 -0400

    Still working on the google maps

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Javascript Entry Point
 import $ from "jquery";
 
+
 $(".tab-container").on("click", "button", function(event){
 	$(".tab-container").addClass("hide");
 	$(event.currentTarget).parent().removeClass("hide");
@@ -9,8 +10,7 @@ $(".tab-container").on("click", "button", function(event){
 var map;
       
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.197047, lng: 18.450762},
-    zoom: 13
-  });
-}
+  map = new google.maps.Map(document.getElementById("map");
+  center: {lat: -34.197047, lng: 18.450762},
+  zoom: 13,
+});
