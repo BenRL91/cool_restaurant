05251113d637e1cbfedeb58122e495cf4bf56867
Mallerie Shirley <mallerie@gmail.com>
2016-03-17 10:16:00 -0400

    More google maps adds.

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,11 @@ $(".tab-container").on("click", "button", function(event){
 	$(".tab-container").addClass("hide");
 	$(event.currentTarget).parent().removeClass("hide");
 });
+
+      
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.197047, lng: 18.450762},
+    zoom: 8
+  });
+}
