21851b18c045f2bb7b2660f32c453a7289c16f5f
Ben Lee <leeohsunshine@gmail.com>
2016-03-19 21:24:06 -0400

    Added to templates.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // Javascript Entry Point
 import $ from "jquery";
 
-
 $(".tab-container").on("click", "button", function(event){
 	$(".tab-container").addClass("hide");
 	$(event.currentTarget).parent().removeClass("hide");
