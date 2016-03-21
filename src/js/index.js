6f82ee5a215abad32152f8ee359d6006dd15b34b
Ben Lee <leeohsunshine@gmail.com>
2016-03-20 22:53:35 -0400

    Reduced JS to one function

--- src/js/index.js
+++ src/js/index.js
@@ -25,10 +25,253 @@ var request = $.ajax({
   url: 'https://json-data.herokuapp.com/restaurant/menu/1'
 });
 // Puts the data drawn from the APIs into workable form //
+// request.then(function(response){
+// appItemTemplate(response.appetizers);
+// entreeItemTemplate(response.entrees);
+// // Puts the data drawn from the special API into the special section //
+//       special.then(function(specResponse) {
+//         response["entrees"].forEach(function(entree){
+// // Checks the id of the menu item againse the special API //
+//   if (entree.id === specResponse.menu_item_id) {
+//         var specHtml = `<span><b>Today's Special</b></span>
+//         <br>       
+//         <img src="./images/sea-scallops-yum.jpg">
+//         <br>
+//         <span>${entree.item}</span>
+//         <span>${entree.price}</span>
+//         <br>
+//         <span>${entree.description}</span>`
+//         $('.specials').append( specHtml );
+//        }
+//     });
+//   });
+// sideItemTemplate(response.sides);
+// });
+// // Creates the HTML for an Array of menu items passed to it //
+// function appItemTemplate(menuItemArray){
+//     menuItemArray.forEach(function(menuItem) {
+//     var title
+//     var allergiesListItem;
+//     var favoriteListItem;
+//     var spicyListItem;
+//     var veganListItem;
+//     if (menuItem.allergies === 1) {
+//       allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
+//     } else {
+//       allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
+//     };
+
+//     if (menuItem.favorite === 1) {
+//       favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
+//     } else {
+//       favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
+//     };
+
+//     if (menuItem.spicy === 1) { 
+//       spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
+//     } else { 
+//       spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
+//     };
+
+//     if (menuItem.vegan === 1) {
+//       veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
+//     } else {
+//       veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
+//     };
+//     if (menuItem === menuItemArray[0]){title = "<h4>Appetizers</h4>"}
+//     else {title = "" }
+//   var menuItemHtml = `
+//     ${title}
+//      <div class="menu-items">
+//        <div class="menulineup">
+//          <h4>${menuItem.item}</h4> 
+//          <h5>$${menuItem.price}</h5>
+//        </div>
+//        <div class="menulineup"><p>${menuItem.description}</p>
+//         <ul class="picky-eaters">
+//          ${allergiesListItem}
+//          ${favoriteListItem}
+//          ${spicyListItem}
+//          ${veganListItem}
+//         </ul>
+//        </div>
+//      </div>`
+//     $('.menu').append(menuItemHtml);
+//   })
+// };
+
+// function entreeItemTemplate(menuItemArray){
+//     menuItemArray.forEach(function(menuItem) {
+//     var title
+//     var allergiesListItem;
+//     var favoriteListItem;
+//     var spicyListItem;
+//     var veganListItem;
+//     if (menuItem.allergies === 1) {
+//       allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
+//     } else {
+//       allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
+//     };
+
+//     if (menuItem.favorite === 1) {
+//       favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
+//     } else {
+//       favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
+//     };
+
+//     if (menuItem.spicy === 1) { 
+//       spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
+//     } else { 
+//       spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
+//     };
+
+//     if (menuItem.vegan === 1) {
+//       veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
+//     } else {
+//       veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
+//     };
+//     if (menuItem === menuItemArray[0]){title = "<h4>Entrees</h4>"}
+//     else {title = "" }
+//   var menuItemHtml = `
+//     ${title}
+//      <div class="menu-items">
+//       <div class="menulineup">
+//        <h4>${menuItem.item}</h4> 
+//        <h5>$${menuItem.price}</h5>
+//       </div>
+//       <div class="menulineup">
+//        <p>${menuItem.description}</p>
+//         <ul class="picky-eaters">
+//          ${allergiesListItem}
+//          ${favoriteListItem}
+//          ${spicyListItem}
+//          ${veganListItem}
+//        </ul>
+//       </div>
+//      </div>`
+//     $('.menu').append(menuItemHtml);
+//   })
+// };
+
+// function sideItemTemplate(menuItemArray){
+//     menuItemArray.forEach(function(menuItem) {
+//     var title
+//     var allergiesListItem;
+//     var favoriteListItem;
+//     var spicyListItem;
+//     var veganListItem;
+//     if (menuItem.allergies === 1) {
+//       allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
+//     } else {
+//       allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
+//     };
+
+//     if (menuItem.favorite === 1) {
+//       favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
+//     } else {
+//       favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
+//     };
+
+//     if (menuItem.spicy === 1) { 
+//       spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
+//     } else { 
+//       spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
+//     };
+
+//     if (menuItem.vegan === 1) {
+//       veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
+//     } else {
+//       veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
+//     };
+//     if (menuItem === menuItemArray[0]){title = "<h4>Sides</h4>"}
+//     else {title = "" }
+//   var menuItemHtml = `
+//     ${title}
+//      <div class="menu-items">
+//      <div class="sidelineup">
+//        <h4>${menuItem.item}</h4> 
+//        <h5>$${menuItem.price}</h5>
+//       </div>
+//       <div class="sidelineup">
+//        <p></p>
+//         <ul class="picky-eaters">
+//          ${allergiesListItem}
+//          ${favoriteListItem}
+//          ${spicyListItem}
+//          ${veganListItem}
+//        </ul>
+//       </div>
+//      </div><br>`
+//     $('.menu').append(menuItemHtml);
+//   })
+// };
 request.then(function(response){
-appItemTemplate(response.appetizers);
-entreeItemTemplate(response.entrees);
-// Puts the data drawn from the special API into the special section //
+var usableResponse = [response.appetizers, response.entrees, response.sides]
+var title, className
+usableResponse.forEach(function(menuItemArray){
+  if (menuItemArray === response["appetizers"]){
+    title = "Appetizers"
+    className = "menulineup"
+  }
+  else if (menuItemArray === response["entrees"]){
+    title = "Entrees"
+    className = "menulineup"
+  }
+  else {
+    title = "Sides"
+    className = "sidelineup"
+  }
+  menuItemArray.forEach(function(menuItem) {
+    var allergiesListItem;
+    var favoriteListItem;
+    var spicyListItem;
+    var veganListItem;
+    if (menuItem.allergies === 1) {
+      allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
+    } else {
+      allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
+    };
+
+    if (menuItem.favorite === 1) {
+      favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
+    } else {
+      favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
+    };
+
+    if (menuItem.spicy === 1) { 
+      spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
+    } else { 
+      spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
+    };
+
+    if (menuItem.vegan === 1) {
+      veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
+    } else {
+      veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
+    };
+    if (menuItem === menuItemArray[0]){title = `<h4>${title}</h4>`}
+    else {title = ""}
+    var menuItemHtml = `
+    ${title}
+     <div class="menu-items">
+     <div class=${className}>
+       <h4>${menuItem.item}</h4> 
+       <h5>$${menuItem.price}</h5>
+      </div>
+      <div class=${className}>
+       <p>${menuItem.description}</p>
+        <ul class="picky-eaters">
+         ${allergiesListItem}
+         ${favoriteListItem}
+         ${spicyListItem}
+         ${veganListItem}
+       </ul>
+      </div>
+     </div><br>`
+        $('.menu').append(menuItemHtml);
+      });;
+  });
+  // Puts the data drawn from the special API into the special section //
       special.then(function(specResponse) {
         response["entrees"].forEach(function(entree){
 // Checks the id of the menu item againse the special API //
@@ -43,167 +286,7 @@ entreeItemTemplate(response.entrees);
         <span>${entree.description}</span>`
         $('.specials').append( specHtml );
        }
+     });
+// //
     });
-  });
-sideItemTemplate(response.sides);
 });
-// Creates the HTML for an Array of menu items passed to it //
-function appItemTemplate(menuItemArray){
-    menuItemArray.forEach(function(menuItem) {
-    var title
-    var allergiesListItem;
-    var favoriteListItem;
-    var spicyListItem;
-    var veganListItem;
-    if (menuItem.allergies === 1) {
-      allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
-    } else {
-      allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
-    };
-
-    if (menuItem.favorite === 1) {
-      favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
-    } else {
-      favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
-    };
-
-    if (menuItem.spicy === 1) { 
-      spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
-    } else { 
-      spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
-    };
-
-    if (menuItem.vegan === 1) {
-      veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
-    } else {
-      veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
-    };
-    if (menuItem === menuItemArray[0]){title = "<h4>Appetizers</h4>"}
-    else {title = "" }
-  var menuItemHtml = `
-    ${title}
-     <div class="menu-items">
-       <div class="menulineup">
-         <h4>${menuItem.item}</h4> 
-         <h5>$${menuItem.price}</h5>
-       </div>
-       <div class="menulineup"><p>${menuItem.description}</p>
-        <ul class="picky-eaters">
-         ${allergiesListItem}
-         ${favoriteListItem}
-         ${spicyListItem}
-         ${veganListItem}
-        </ul>
-       </div>
-     </div>`
-    $('.menu').append(menuItemHtml);
-  })
-};
-
-function entreeItemTemplate(menuItemArray){
-    menuItemArray.forEach(function(menuItem) {
-    var title
-    var allergiesListItem;
-    var favoriteListItem;
-    var spicyListItem;
-    var veganListItem;
-    if (menuItem.allergies === 1) {
-      allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
-    } else {
-      allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
-    };
-
-    if (menuItem.favorite === 1) {
-      favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
-    } else {
-      favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
-    };
-
-    if (menuItem.spicy === 1) { 
-      spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
-    } else { 
-      spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
-    };
-
-    if (menuItem.vegan === 1) {
-      veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
-    } else {
-      veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
-    };
-    if (menuItem === menuItemArray[0]){title = "<h4>Entrees</h4>"}
-    else {title = "" }
-  var menuItemHtml = `
-    ${title}
-     <div class="menu-items">
-      <div class="menulineup">
-       <h4>${menuItem.item}</h4> 
-       <h5>$${menuItem.price}</h5>
-      </div>
-      <div class="menulineup">
-       <p>${menuItem.description}</p>
-        <ul class="picky-eaters">
-         ${allergiesListItem}
-         ${favoriteListItem}
-         ${spicyListItem}
-         ${veganListItem}
-       </ul>
-      </div>
-     </div>`
-    $('.menu').append(menuItemHtml);
-  })
-};
-
-function sideItemTemplate(menuItemArray){
-    menuItemArray.forEach(function(menuItem) {
-    var title
-    var allergiesListItem;
-    var favoriteListItem;
-    var spicyListItem;
-    var veganListItem;
-    if (menuItem.allergies === 1) {
-      allergiesListItem = `<li class="allergies allergic"><a href="#" title="This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance."><img src="./images/allergies.png"></a></li>`
-    } else {
-      allergiesListItem = `<li class="allergies greyed"><a href="#"><img src="./images/allergies.png"></a></li>`
-    };
-
-    if (menuItem.favorite === 1) {
-      favoriteListItem = `<li class="favorite is-favorite"><a href="#" title="We have been doing this a long time and this item has become one of our favorites."><img src="./images/favorite.png"></a></li>`
-    } else {
-      favoriteListItem = `<li class="favorite greyed"><a href="#"><img src="./images/favorite.png"></a></li>`
-    };
-
-    if (menuItem.spicy === 1) { 
-      spicyListItem = `<li class="spicy is-spicy"><a href="#" title="This item is spicy, please handle with care and drink lots of water."><img src="./images/spicy.png"></a></li>`
-    } else { 
-      spicyListItem = `<li class="spicy greyed"><a href="#"><img src="./images/spicy.png"></a></li>`
-    };
-
-    if (menuItem.vegan === 1) {
-      veganListItem = `<li class="vegan is-vegan"><a href="#" title="This item contains no meat and has been prepared without the use of animal products."><img src="./images/vegan.png"></a></li>`
-    } else {
-      veganListItem = `<li class="vegan greyed"><a href="#"><img src="./images/vegan.png"></a></li>`
-    };
-    if (menuItem === menuItemArray[0]){title = "<h4>Sides</h4>"}
-    else {title = "" }
-  var menuItemHtml = `
-    ${title}
-     <div class="menu-items">
-     <div class="sidelineup">
-       <h4>${menuItem.item}</h4> 
-       <h5>$${menuItem.price}</h5>
-      </div>
-      <div class="sidelineup">
-       <p></p>
-        <ul class="picky-eaters">
-         ${allergiesListItem}
-         ${favoriteListItem}
-         ${spicyListItem}
-         ${veganListItem}
-       </ul>
-      </div>
-     </div><br>`
-    $('.menu').append(menuItemHtml);
-  })
-};
-
-// Appetizers Entrees Sides
